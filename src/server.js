42872b57422dd6faba0fb8e2af3a5fc806834f8a
Sharon Tsai <sharonmctsai@gmail.com>
2024-04-11 22:55:42 +0100

    servcer

--- src/server.js
+++ src/server.js
@@ -89,7 +89,7 @@ async function init() {
   });
   server.auth.default("session");
 
-  db.init("mongo");
+  db.init("json");
   server.route(webRoutes);
   server.route(apiRoutes);
   await server.start();
